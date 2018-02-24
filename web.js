aae7ad2af026f311c87a2934b3d07bba908cb767
Grant Ramil <grant.ramil@veeva.com>
2018-02-23 21:42:19 -0800

    Remove express.js harness (used for local testing) and images from git repo

--- web.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('pages'));
-app.use(express.static('styles'));
-app.use(express.static('images'));
-
-app.listen(3000);
